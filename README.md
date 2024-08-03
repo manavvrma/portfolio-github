6cb64f845c9431c9483ec3c55182e05c2081e7a5
Manav Verma <manavvrma17@gmail.com>
2024-08-04 00:54:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+  <h2 align="center">Manav - Portfolio Website</h2>
+
+  A fully responsive personal portfolio website, responsive for all devices, build using HTML, CSS, and JavaScript.
+
+
---
+++ asset/js/server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post("/send-email", (req, res) => {
+  const { name, email_address, message } = req.body;
+
+  const mailOptions = {
+    from: email_address,
+    to: process.env.EMAIL_USER,
+    subject: "New Message from Contact Form",
+    text: `Name: ${name}\nEmail: ${email_address}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error sending email:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
